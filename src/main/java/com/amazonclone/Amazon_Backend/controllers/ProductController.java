193c7a2749d9794da2e8ecbef24641602ba6ff02
vivek kumar gupta <vivekgp8899@gmail.com>
2023-08-28 17:49:43 +0530

    product controller done

---
+++ src/main/java/com/amazonclone/Amazon_Backend/controllers/ProductController.java
@@ -0,0 +1,132 @@
+package com.amazonclone.Amazon_Backend.controllers;
+
+import java.io.IOException;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonclone.Amazon_Backend.config.AppConstants;
+import com.amazonclone.Amazon_Backend.dto.ProductDTO;
+import com.amazonclone.Amazon_Backend.dto.ProductResponse;
+import com.amazonclone.Amazon_Backend.entities.Product;
+import com.amazonclone.Amazon_Backend.services.ProductService;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.validation.Valid;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+	
+
+	@Autowired
+	private ProductService productService;
+	
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	
+	@PostMapping("/admin/categories/{categoryId}/product/data/image")
+	public ResponseEntity<ProductDTO> addProduct(@PathVariable Long categoryId,@RequestParam ("data") String data,@RequestParam("image") MultipartFile image) throws IOException {
+		
+		Product product = mapper.readValue(data, Product.class);
+		ProductDTO savedProduct = productService.addProduct(categoryId, product, image);
+
+		return new ResponseEntity<ProductDTO>(savedProduct, HttpStatus.CREATED);
+	}
+	
+	
+	@GetMapping("/public/products")
+	public ResponseEntity<ProductResponse> getAllProductsPage(
+			@RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+			@RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+			@RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_PRODUCTS_BY, required = false) String sortBy,
+			@RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
+
+		ProductResponse productResponse = productService.getAllProductsByPage(pageNumber, pageSize, sortBy, sortOrder);
+
+		return new ResponseEntity<ProductResponse>(productResponse, HttpStatus.FOUND);
+	}
+	
+	
+	
+	@GetMapping("/public/categories/{categoryId}/products")
+	public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId,
+			@RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+			@RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+			@RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_PRODUCTS_BY, required = false) String sortBy,
+			@RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
+
+		ProductResponse productResponse = productService.searchByCategory(categoryId, pageNumber, pageSize, sortBy,
+				sortOrder);
+
+		return new ResponseEntity<ProductResponse>(productResponse, HttpStatus.FOUND);
+	}
+	
+	
+	@GetMapping("/public/products/keyword/{keyword}")
+	public ResponseEntity<ProductResponse> getProductsByKeyword(@PathVariable String keyword,
+			@RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+			@RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+			@RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_PRODUCTS_BY, required = false) String sortBy,
+			@RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
+
+		ProductResponse productResponse = productService.searchProductByKeyword(keyword, pageNumber, pageSize, sortBy,
+				sortOrder);
+
+		return new ResponseEntity<ProductResponse>(productResponse, HttpStatus.FOUND);
+	}
+	
+//	
+//	@PutMapping("/admin/products/{productId}")
+//	public ResponseEntity<ProductDTO> updateProduct(@RequestBody Product product,
+//			@PathVariable Long productId) {
+//		ProductDTO updatedProduct = productService.updateProduct(productId, product);
+//
+//		return new ResponseEntity<ProductDTO>(updatedProduct, HttpStatus.OK);
+//	}
+//	
+	@PutMapping("/admin/products/{productId}/image")
+	public ResponseEntity<ProductDTO> updateProductImage(@PathVariable Long productId, @RequestParam("image") MultipartFile image) throws IOException {
+		ProductDTO updatedProduct = productService.updateProductImage(productId, image);
+
+		return new ResponseEntity<ProductDTO>(updatedProduct, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/admin/products/{productId}")
+	public ResponseEntity<String> deleteProductByCategory(@PathVariable Long productId) {
+		String status = productService.deleteProduct(productId);
+
+		return new ResponseEntity<String>(status, HttpStatus.OK);
+	}
+	
+	
+	//All prodcut list without pagination
+//	@GetMapping("/public/products")
+//	public ResponseEntity<List<ProductDTO>> getAllProducts(){
+//
+//		List<ProductDTO> productlist = productService.getAllProducts();
+//
+//		return new ResponseEntity<List<ProductDTO>>(productlist, HttpStatus.FOUND);
+//	}
+	
+}
